1f66a88514327b531b1a9ebc18cd720c9f38530a
dark-mirage <username@users.noreply.github.com>
2025-11-28 18:39:05 +0500

    some changes

--- src/app/hooks/useBuy.ts
+++ src/app/hooks/useBuy.ts
@@ -1,4 +1,3 @@
-
 import { BEGIN_BUY, CREATE_ORDER } from '../graphql/mutations';
 import { useMutation, useQuery } from '@apollo/client/react';
 import { ORDER_CARTS } from '../graphql/queries';
@@ -8,11 +7,36 @@ export interface ProductBuy {
   count: number;
 }
 
+// Добавьте интерфейсы для типизации
+interface CreateOrderResponse {
+  createUpdateOrder: {
+    pk: number;
+  };
+}
+
+interface CreateOrderVariables {
+  pk?: number | null;
+  order: {
+    name?: string;
+    surname?: string;
+    email?: string;
+    phone?: string;
+    country?: string;
+    city?: string;
+    street?: string;
+    apartment?: string;
+    zipCode?: string;
+    deliveryNotes?: string;
+    deliveryType?: string;
+  };
+}
+
 export const useBeginBuy = () => {
   return useMutation(BEGIN_BUY, { errorPolicy: "all" });
 };
+
 export const useCreateOrder = () => {
-  return useMutation(CREATE_ORDER, { errorPolicy: "all" });
+  return useMutation<CreateOrderResponse, CreateOrderVariables>(CREATE_ORDER, { errorPolicy: "all" });
 };
 
 export const useOrderCarts = () => {
