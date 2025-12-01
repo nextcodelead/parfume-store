7480a39d06f7a87faad2a5ad157a8c0cd8376899
dark-mirage <username@users.noreply.github.com>
2025-12-01 11:15:23 +0500

    some changes

--- src/app/hooks/useBrands.ts
+++ src/app/hooks/useBrands.ts
@@ -2,11 +2,19 @@ import { useQuery } from '@apollo/client/react';
 import { ADMIN_CREATE_PRODUCT_SELECT_BRAND } from '../graphql/queries';
 import { useIsClient } from './useIsClient';
 
+interface Brand {
+  pk: number;
+  name: string;
+}
 
+interface SelectBrandsData {
+  brands: Brand[];
+}
 
 export const useSelectBrands = () => {
   const isClient = useIsClient();
-  return useQuery(ADMIN_CREATE_PRODUCT_SELECT_BRAND, {
+
+  return useQuery<SelectBrandsData>(ADMIN_CREATE_PRODUCT_SELECT_BRAND, {
     variables: {},
     fetchPolicy: 'cache-first',
     errorPolicy: 'all',
